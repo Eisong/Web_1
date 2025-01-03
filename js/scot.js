0ac8ac29a5779a5efd0c5635add33a9881890831
Edet Isong <125439787+Eisong@users.noreply.github.com>
2025-01-03 09:14:22 +0100

    Delete js directory

--- js/scot.js
+++
@@ -1,8 +0,0 @@
-function sign() {
-	document.getElementById('login').style.display = "none";
-	document.getElementById('sign').style.display = "inherit";
-}
-function log() {
-	document.getElementById('login').style.display = "inherit";
-	document.getElementById('sign').style.display = "none";
-}
